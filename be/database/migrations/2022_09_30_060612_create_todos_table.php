f65c8de12b173fa5b36595c881ad4b451b65f6c6
Mikhail <mikhail.davlichin@kaspersky.com>
2022-09-30 09:22:09 +0300

    be init

---
+++ be/database/migrations/2022_09_30_060612_create_todos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('done')->default(false);
+            $table->string('description');
+            $table->unsignedInteger('viewed');
+            $table->unsignedInteger('project_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
